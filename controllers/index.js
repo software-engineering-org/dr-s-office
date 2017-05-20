e578436a5ec23379073e69a3e926471bc4cd9253
hesham <heshammagdy1995@gmail.com>
2017-05-20 22:18:10 +0200

    login and sign up works correctly

--- controllers/index.js
+++ controllers/index.js
@@ -13,9 +13,71 @@ function retRouter(auth) {
     //doctor has 2 forms of GET and 1 form of POST
     //receptionist has 1 form of GET
 
-    router.get('/', auth.isLoggedIn,
+    router.get('/',
         function (req, res) {
-            res.render("recep");
+            res.render("patient",{
+                data1:
+                    { P_Name: 'dd',
+                        P_city: 'rr',
+                        P_ZipCode: 21345,
+                        P_BirthDate: '2008-08-08',
+                        P_Phone: '9999988',
+                        P_Email: 'asd@dd.com',
+                        P_AdmissionDate: '2008-08-08',
+                        P_SSN: '2163',
+                        P_Image: 'image' },
+                data2:[
+                    {
+                        "B_Cost": 1000,
+                        "D_Description": "d44",
+                        "T_Description": "t44",
+                        "S_Description": "s44",
+                        "BillItems": [
+                            {
+                                "BI_Description": "bill 1 ",
+                                "BI_Name": "bill 1 ",
+                                "BI_UnitCost": 20
+                            },
+                            {
+                                "BI_Description": "bill 2",
+                                "BI_Name": "bill 2",
+                                "BI_UnitCost": 20
+                            }
+                        ],
+                        "DiagItems": [
+                            {
+                                "DI_Description": "Diag 1",
+                                "DI_Name": "Diag 1"
+                            },
+                            {
+                                "DI_Description": "Add_BillingItem",
+                                "DI_Name": "Add_DiagItem"
+                            }
+                        ],
+                        "TreatItems": [
+                            {
+                                "TI_Description": "treat 1 ",
+                                "TI_Name": "treat 1"
+                            },
+                            {
+                                "TI_Description": "treat 2",
+                                "TI_Name": "treat 2"
+                            }
+                        ],
+                        "SymItems": [
+                            {
+                                "SI_Description": "sym 1d",
+                                "SI_Name": "sym 1 "
+                            },
+                            {
+                                "SI_Description": "sym 2d",
+                                "SI_Name": "sym 2"
+                            }
+                        ]
+                    }
+                ]
+
+            });
            // res.send("hello"+ req.session.type);
         });
 
---
+++ temp.js
@@ -0,0 +1,5 @@
+/**
+ * Created by -HRW- on 5/20/2017.
+ */
+
+data2[0].SymItems[i].SI_Name
